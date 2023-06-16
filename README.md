527a555c095e2cf539f376fe6b1a4530fe118754
cehnhy <cehnhy@outlook.com>
2023-06-16 11:15:19 +0800

    packer -> lazy

--- README.md
+++ README.md
@@ -4,20 +4,6 @@ cehnhy's neovim config
 
 ## Install
 
-> Clone Repository
-
 ```shell
 git clone git@github.com:cehnhy/nvim.git ~/.config/nvim
-```
-
-> Install Packer
-
-```shell
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-```
-
-> Install Plugins
-
-```txt
-:PackerSync
 ```
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,55 +1,86 @@
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
 
-    -- Colorscheme
-    -- https://github.com/projekt0n/github-nvim-theme
-    use({
+return require("lazy").setup({
+    {
+        -- Colorscheme
+        -- https://github.com/projekt0n/github-nvim-theme
         'projekt0n/github-nvim-theme',
+        version = "*",
         config = function()
             require('config.github-theme')
-        end
-    })
-
-    -- File Explorer
-    -- https://github.com/nvim-tree/nvim-tree.lua
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons',
+        end,
+    },
+    {
+        -- File Explorer
+        -- https://github.com/nvim-tree/nvim-tree.lua
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
         },
         config = function()
-            require('config.nvim-tree')
-        end
-    }
-
-    -- Tabline
-    -- https://github.com/akinsho/bufferline.nvim
-    use {
+            require("config.nvim-tree")
+        end,
+    },
+    {
+        -- Tabline
+        -- https://github.com/akinsho/bufferline.nvim
         'akinsho/bufferline.nvim',
-        tag = "*",
-        requires = {
+        version = "*",
+        dependencies = {
             'nvim-tree/nvim-web-devicons',
         },
         config = function()
             require('config.bufferline')
-        end
-    }
-
-    -- Statusline
-    -- https://github.com/nvim-lualine/lualine.nvim
-    use {
+        end,
+    },
+    {
+        -- Statusline
+        -- https://github.com/nvim-lualine/lualine.nvim
         'nvim-lualine/lualine.nvim',
-        requires = {
+        version = "*",
+        dependencies = {
             'nvim-tree/nvim-web-devicons',
-            opt = true
         },
         config = function()
             require('config.lualine')
-        end
+        end,
+    },
+    {
+        -- Snytax Highlight
+        -- https://github.com/nvim-treesitter/nvim-treesitter
+        'nvim-treesitter/nvim-treesitter',
+        version = "*",
+        build = ":TSUpdate",
+        config = function()
+            require('config.nvim-treesitter')
+        end,
+    },
+    {
+        -- LSP
+        -- https://github.com/williamboman/mason.nvim
+        "williamboman/mason.nvim",
+        version = "*",
+        build = ":MasonUpdate",
+        config = function()
+            require('config.mason')
+        end,
+    },
+    {
+        -- GO
+        -- https://github.com/fatih/vim-go
+        'fatih/vim-go',
+        version = "*",
     }
-
-    -- GO
-    -- https://github.com/fatih/vim-go
-    use { 'fatih/vim-go' }
-end)
+}, {})
